7e030a8330f1701bc3561c172078965455322190
angel9015 <dev10387512@gmail.com>
2021-03-04 00:03:56 +0300

    aasm

--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -1,4 +1,5 @@
 class LandingPage < ApplicationRecord
+  include AASM
   searchkick
   enum status: {
     draft: 'draft',
@@ -9,4 +10,24 @@ class LandingPage < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'last_updated_by'
   validates :page_key, :page_url, :page_title, :meta_description, :title, :content, presence: true
   validates :page_key, :page_url, :page_title, uniqueness: true
+
+
+  aasm column: 'status' do
+    state :pending, initial: true
+    state :pending_approval
+    state :approved
+    state :rejected
+
+    event :pending_review do
+      transitions from: :pending, to: :pending_approval
+    end
+
+    event :approve, after_commit: :send_status_notification do
+      transitions from: :pending_approval, to: :approved
+    end
+
+    event :reject, after_commit: :send_status_notification do
+      transitions from: :pending_approval, to: :rejected
+    end
+  end
 end
