d4a63ef3729c20a27f6318677d1f7c25f3e75245
angel9015 <dev10387512@gmail.com>
2021-04-12 21:33:59 -0700

    Update studio.rb

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -45,7 +45,8 @@ class Studio < ApplicationRecord
 
   validates :avatar, :hero_banner, size: { less_than: 10.megabytes, message: 'is not given between size' }
 
-  validates :email, presence: true, on: create
+  validates :email, presence: true, on: :create
+  validates :name, presence: true
   validates :user_id, uniqueness: true
 
   after_commit :upgrade_user_role, on: :create
