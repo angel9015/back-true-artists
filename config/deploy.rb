eecdf26127126976b477cd5d4144b1512c08ef7c
angel9015 <dev10387512@gmail.com>
2021-04-28 13:33:44 -0700

    cleaning deploy environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 lock '~> 3.14.1'
 
 set :ssh_options, {
@@ -13,19 +12,19 @@ set :use_sudo, false
 set :rails_env, 'production'
 
 set :pty, true
-set :copy_exclude, ['.git']
-set :stages, %w[production staging]
-set :default_stage, 'staging'
+set :copy_exclude, [".git"]
+set :stages, %w(staging production)
+set :default_stage, "staging"
 set :default_shell, '/bin/bash -l'
-set :sidekiq_default_hooks, true
+# set :sidekiq_default_hooks, false
 
-SSHKit.config.command_map[:sidekiq] = 'bundle exec sidekiq'
-SSHKit.config.command_map[:sidekiqctl] = 'bundle exec sidekiqctl'
+SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
+SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
 
-set :whenever_identifier, -> { "#{fetch(:worker)}_#{fetch(:stage)}" }
+set :whenever_identifier, ->{ "#{fetch(:worker)}_#{fetch(:stage)}" }
 
-set :application, 'trueartists'
-set :repo_url, 'git@github.com:jwachira/trueartists.git'
+set :application, "trueartists"
+set :repo_url, "git@github.com:jwachira/trueartists.git"
 set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 set :migration_role, :db
 set :conditionally_migrate, true
@@ -35,10 +34,15 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :keep_releases, 5
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml',
-                                                 '.env')
+  '.env',
+  'config/storage.yml',
+  'config/secrets.yml'
+)
 append :linked_files,
-       'config/database.yml',
-       '.env'
+  'config/database.yml',
+  '.env',
+  'config/amazon_s3.yml',
+  'config/secrets.yml'
 
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'tmp/pids',
@@ -50,17 +54,14 @@ set :linked_dirs, fetch(:linked_dirs, []).push(
 )
 
 set :rollbar_token, 'bf76b421739a47288ce7eefbcaafd9aa'
-set :rollbar_env, proc { fetch :stage }
-set :rollbar_role, proc { :app }
+set :rollbar_env, Proc.new { fetch :stage }
+set :rollbar_role, Proc.new { :app }
 
 set :passenger_restart_with_touch, true
 set :passenger_restart_options, -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
 
-# set :rvm_ruby_version, '2.6.1@261'
+set :rvm_ruby_version, '2.6.1@261'
 set :bundle_flags, '--deployment'
 set :maintenance_roles, -> { roles([:web]) }
-set :maintenance_template_path, File.expand_path('../app/views/layouts/maintenance.html.erb', __dir__)
+set :maintenance_template_path, File.expand_path("../../app/views/layouts/maintenance.html.erb", __FILE__)
 set :maintenance_dirname, -> { "#{current_path}/public/system" }
-
-# after :deploy, 'notify_rollbar'
-# after "deploy:migrate", 'db:migrate:others'
