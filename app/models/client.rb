986b6595009126d88649bceb5222f8d4f79a20fb
angel9015 <dev10387512@gmail.com>
2021-08-06 10:21:55 -0700

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,10 +3,10 @@ class Client< ApplicationRecord
   belongs_to :studio, optional: true
   validates :name, :email, :phone_number, presence: true
 
-  def self.search(options = {})
+  def self.search(query)
     where("name LIKE :query OR
            phone_number LIKE :query OR
            email LIKE :query",
-           query: "%#{options[:query]}%")
+           query: "%#{query}%")
   end
 end
